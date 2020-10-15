180eb298a6bbd43b6bf55a81af0b466f99c31836
Mirco serra <48206399+Jirkunow@users.noreply.github.com>
2020-10-15 14:12:55 +0200

    il file aggiunge media in cima a data_all.csv

--- add_mean.py
+++ add_mean.py
@@ -5,20 +5,38 @@ dataset = pd.read_csv('csv/data_all.csv')
 
 count = 0
 avg = 0
-indexes = dataset.head()
+indexes = dataset.columns
 mean_vector_load = []
 vector_of_mean = []
 
 for i in range(len(indexes)-2):
-    mean_vector_load = dataset.iloc[j+2,i]
-    for j in  range(len(mean_vector_load)):
-        if mean_vector_load[j + 2 , i] > 0 and j+2%5 != 2:
-            avg = mean_vector_load[j + 2 , i]
-            count += 1;
-        elif j+2%5 != 2:
-            avg = mean_vector_load[j + 2, i]
-            count += 1;
+    mean_vector_load = dataset.iloc[:, i+2].values
+    for j in range(len(mean_vector_load)):
+        if int(mean_vector_load[j]) < 1:
+            avg += mean_vector_load[j]
+            count += 1
+        elif i % 5 != 1:
+            avg += mean_vector_load[j]
+            count += 1
     avg = avg/count
-    vector_of_mean[j] = avg
+    vector_of_mean.append(avg)
 
-vector_of_mean.to_csv('csv/data_all.csv', mode='a', header=False)
+#vector_of_mean.to_csv('csv/data_all.csv', mode='a', header=False)
+
+fst_two_columns = [999999, 'Uomo Medio']
+
+for val in vector_of_mean:
+    fst_two_columns.append(val)
+
+df_fst_cl = pd.DataFrame(fst_two_columns)
+
+df_fst_cl.index = indexes.to_list()
+
+df_fst_cl.append(dataset)
+
+df_fst_cl = df_fst_cl.transpose()
+
+df_fst_cl.to_csv('mean.csv', index=False)
+
+df_fst_cl.append(dataset).head().to_csv('data_all_with_mean.csv', index=False)
+
